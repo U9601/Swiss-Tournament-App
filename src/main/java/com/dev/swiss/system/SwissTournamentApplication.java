936afae12aee92f46f448063eb69989eb5ea6f03
U9601 <ctjones1965@gmail.com>
2022-11-26 21:32:37 +0000

    Initial Commit

---
+++ src/main/java/com/dev/swiss/system/SwissTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.dev.swiss.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwissTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwissTournamentApplication.class, args);
+	}
+
+}
